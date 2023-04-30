074726b2fb9924223dbee956fa0ac17131158c67
Anastasiia Solomka <solomken@gmail.com>
2023-04-30 19:26:58 +0200

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Anastasiia Solomka on 30.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
