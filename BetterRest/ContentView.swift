b670a777b25a7926477879fd21affe5cfd2b4304
Anastasiia Solomka <solomken@gmail.com>
2023-05-04 14:00:59 +0200

    ui and logic updates after review

--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -9,46 +9,11 @@ import CoreML
 import SwiftUI
 
 struct ContentView: View {
-    @State private var wakeUp = Date.now
+    @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
-    @State private var coffeeAmount = 1
+    @State private var coffeeAmount = 0
     
-    @State private var alertTitle = ""
-    @State private var alertMessage = ""
-    @State private var showingAlert = false
-    
-    var body: some View {
-        NavigationView {
-            VStack {
-                Text("When do you want to wake up?")
-                    .font(.headline)
-                
-                DatePicker("Please enter time", selection: $wakeUp, displayedComponents: .hourAndMinute)
-                    .labelsHidden()
-                
-                Text("Desired amount of sleep")
-                    .font(.headline)
-                
-                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.2)
-                
-                Text("Daily coffee intake")
-                    .font(.headline)
-                
-                Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
-            }
-            .navigationTitle("BetterRest")
-            .toolbar {
-                Button("Calculate", action: calculateBedtime)
-            }
-            .alert(alertTitle, isPresented: $showingAlert) {
-                Button("Ok") { }
-            } message: {
-                Text(alertMessage)
-            }
-        }
-    }
-    
-    func calculateBedtime() {
+    var calculatedSleepTime: String {
         do {
             let config = MLModelConfiguration()
             let model = try SleepCalculator(configuration: config)
@@ -58,17 +23,63 @@ struct ContentView: View {
             let minute = (components.minute ?? 0) * 60 //number of minutes in seconds
             
             let prediction = try model.prediction(wake: Double(hour + minute), estimatedSleep: sleepAmount, coffee: Double(coffeeAmount)) //calculate how much sleep they actually need
-            
             let sleepTime = wakeUp - prediction.actualSleep
             
-            alertTitle = "Your ideal bedtime is..."
-            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            return "\(sleepTime.formatted(date: .omitted, time: .shortened))"
+                
         } catch {
-            alertTitle = "Error"
-            alertMessage = "Sorry, it was a problem calculating your bedtime"
+            return "There was an error"
         }
+    }
+    
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
         
-        showingAlert = true
+        return Calendar.current.date(from: components) ?? Date.now
+    }
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section("When do you want to wake up?") {
+                    DatePicker("Please enter time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                }
+                .font(.headline)
+                
+                Section("Desired amount of sleep") {
+                    Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.2)
+                }
+                .font(.headline)
+                
+                Section("Daily coffee intake") {
+                    Picker("Coffee amount", selection: $coffeeAmount) {
+                        ForEach(1..<21) {
+                            if $0 == 1 {
+                                Text("\($0) cup")
+                            } else {
+                                Text("\($0) cups")
+                            }
+                        }
+                    }
+                    /*
+                     Stepper(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups", value: $coffeeAmount, in: 1...20)
+                     */
+                }
+                .font(.headline)
+                
+                Section("Your perfect bedtime is") {
+                    Text("\(calculatedSleepTime)")
+                        .font(.largeTitle)
+                }
+                .font(.headline)
+                .foregroundColor(.blue)
+                
+            }
+            .navigationTitle("BetterRest")
+        }
     }
 }
 
